11aa028af8b807b1862ee871b1ae2fe521827b51
Aleksandr Stepanov <i@eerie.su>
2013-06-24 13:19:14 +0400

    parent

--- parser/parent.py
+++ parser/parent.py
@@ -1,21 +1,40 @@
+import time
+
 __author__ = 'eri'
 
 import settings
 from multiprocessing import Process, Queue, current_process
+from child import Parser
+
+
 
 def parser(host,done_queue):
-    pass
+    Parser(host,done_queue).run()
 
 
-def
+def flush(t,results):
+    t = time.strftime("%a, %d %b %Y %H:%M:%S",t)
+    with open('done.log','a') as log:
+        lines = []
+        for host in results.keys():
+            avg = sum(results[host])/float(len(results[host]))
+            lines.append("[%s] [%s]: average=%f " % (t,host,avg))
+        log.writelines(lines)
+
+
+
 
 def main():
     done_queue = Queue()
+
     results = {}
     for host in settings.machines:
         results[host] = []
         Process(target=parser,args=(host,done_queue)).start()
-
+    t = time.time()
     while True:
         host,done_time = done_queue.get()
         results[host].append(done_time)
+        if time.time() - t > 60*60 :
+            t = time.time()
+            flush(t,results)
